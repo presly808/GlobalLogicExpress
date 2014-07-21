1b86d837509da6aaa6380197d071967bf3907593
presly808 <presly808@gmail.com>
2014-07-21 09:25:18 +0300

    test commit

---
+++ src/artcode/day7/TestEquals.java
@@ -0,0 +1,15 @@
+package artcode.day7;
+
+/**
+ * Created by admin on 21.07.2014.
+ */
+public class TestEquals {
+
+    public static void main(String[] args) {
+
+        System.out.println("test");
+
+    }
+
+
+}
